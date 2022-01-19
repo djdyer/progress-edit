40972a28b2824bb514b738ea6f0ea9bbee3e0433
David Dyer <davidrossdyer@gmail.com>
2022-01-19 11:21:51 -0500

    updates to package.json, editor.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,10 +1,10 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
+// Import methods to get and save data from indexedDB database
 import { getDb, putDb } from "./database";
 import { header } from "./header";
 
 export default class {
   constructor() {
-    const localData = localStorage.getItem("content");
+    const localData = localStorage.getItem("edits");
 
     // check if CodeMirror is loaded
     if (typeof CodeMirror === "undefined") {
@@ -30,13 +30,13 @@ export default class {
     });
 
     this.editor.on("change", () => {
-      localStorage.setItem("content", this.editor.getValue());
+      localStorage.setItem("edits", this.editor.getValue());
     });
 
     // Save the content of the editor when the editor itself is loses focus
     this.editor.on("blur", () => {
       console.log("The editor has lost focus");
-      putDb(localStorage.getItem("content"));
+      putDb(localStorage.getItem("edits"));
     });
   }
 }
